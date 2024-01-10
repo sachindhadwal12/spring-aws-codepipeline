b2ed922f340625e01e06ad05ce5315886caebcac
SachinDhadwal122 <sachin.dhadwal@niit.com>
2024-01-10 10:27:12 +0530

    buildspec updated with new data

--- src/main/java/com/stackroute/springbootawsdeploy/SpringbootAwsDeployApplication.java
+++ src/main/java/com/stackroute/springbootawsdeploy/SpringbootAwsDeployApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootAwsDeployApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAwsDeployApplication.class, args);
 	}
